6529e47dcf8f63f138a06b780c36d9d4dd52792f
rnsuka <rnsuka@ogw-pc13253.wnet.local>
2016-12-07 14:59:38 +0000

    Progressos

--- src/resources/js/scripts.js
+++ src/resources/js/scripts.js
@@ -1,16 +1,19 @@
 function paintCell(cell) {
 	var cellValue = cell.getAttribute("value");
 	if(cellValue >= 500) {
-		cell.style.backgroundColor = "red";
+		cell.style.backgroundColor = "#ff0000";
 	} else if(cellValue >= 250 && cellValue < 500) {
-		cell.style.backgroundColor = "yellow";
+		cell.style.backgroundColor = "#999999";
 	} else if(cellValue == 0) {
 		cell.style.backgroundColor = "#00ff00";
 	}
 }
 
-var severeCell = document.getElementsByClassName("severeHits");
-for(i = 0; i < severeCell.length; i++) {
-	paintCell(severeCell[i]);
+var severeHits = document.getElementsByClassName("severeHits");
+var severeType = document.getElementsByClassName("severeType");
+for(i = 0; i < severeHits.length; i++) {
+	paintCell(severeHits[i]);
 }
-
+for(i = 0; i < severeType.length; i++) {
+	paintCell(severeType[i]);
+}
