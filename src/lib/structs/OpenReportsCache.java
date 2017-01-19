92e83bc9d5becafb0824d55e1724b8fcd377921c
rnsuka <rnsuka@ogw-pc13253.wnet.local>
2017-01-19 17:36:51 +0000

    OpenReportCache.java created

---
+++ src/lib/structs/OpenReportsCache.java
@@ -0,0 +1,25 @@
+package lib.structs;
+
+import lib.email.EmailManager;
+
+public class OpenReportsCache {
+	private EmailManager emailList;
+	
+	
+	
+	public OpenReportsCache() {
+		init();
+	}
+	
+	private void init() {
+		emailList = new EmailManager();
+	}
+	
+	public EmailManager getEmailList()  {
+		return this.emailList;
+	}
+	
+	
+	
+	
+}
